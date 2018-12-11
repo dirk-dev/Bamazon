8539c2090965bd691bb9312e9d28ca16179e5229
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-12-11 17:03:51 -0500

    updated JS file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,64 +28,67 @@ connection.connect(function (err) {
 
 function inventoryDisplay() {
     connection.query("SELECT * FROM products", function (err, results) {
-        if (err) throw err;
-        console.log("Items available for sale:");
-        console.table(results);
-        console.log("---------------------------------------------------------");
+            if (err) throw err;
+            console.log("Items available for sale:");
+            console.table(results);
+            console.log("---------------------------------------------------------");
 
-        inquirer.prompt([{
-                    name: 'choice',
-                    type: 'input',
-                    // choices: function () {
-                    //     var productArray = [];
-                    //     for (var i = 0; i < results.length; i++) {
-                    //         /* changing this to item_id results in an unhandled promise rejection warning */
-                    //         productArray.push(results[i].product_name);
-                    //     }
-                    //     console.log('prodArray:', productArray)
-                    //     return productArray;
-                    // },
-                    message: 'What is the id of the product you want to purchase?',
-                    validate: function (value) {
-                        if (isNaN(value) === false) {
-                            return true;
+            inquirer.prompt([{
+                        name: 'choice',
+                        type: 'input',
+                        message: 'What is the id of the product you want to purchase?',
+                        validate: function (value) {
+                            //prevents user from entering invalid input
+                            if (value > results.length) {
+                                return false;
+                            }
+                            if (value < 1) {
+                                return false;
+                            }
+                            if (isNaN(value) === false) {
+                                return true;
+                            }
+                            return false;
                         }
-                        return false;
-                    }
-                },
-                {
-                    name: 'units_to_buy',
-                    type: 'input',
-                    message: 'How many units do want to buy?',
-                    validate: function (value) {
-                        if (isNaN(value) === false) {
-                            return true;
+                    },
+                    {
+                        name: 'units_to_buy',
+                        type: 'input',
+                        message: 'How many units do want to buy?',
+                        validate: function (value) {
+                            if (value < 1) {
+                                return false;
+                            }
+                            if (isNaN(value) === false) {
+                                return true;
+                            }
+                            return false;
                         }
-                        return false;
                     }
-                }
 
-            ])
-            .then(function (answer) {
-                var chosenItem;
-                console.log("a.choice", answer.choice)
-                for (var i = 0; i < results.length; i++) {
-                    if (answer.choice === results[i].id) {
-                        chosenItem = results[i];
-                        console.log('chosen item', chosenItem);
+                ])
+                .then(function (answer) {
+
+                        console.log("a.choice", answer.choice)
+                        var chosenItem = answer.choice;
+
+                        // for (var i = 0; i < results.length; i++) {
+                        //     if (answer.choice === results[i].id) {
+                        //         chosenItem = results[i];
+                        //         console.log('chosen item', chosenItem);
+                        //     }
                     }
-                }
 
-                // if (chosenItem.stock_quantity <= answer.units_to_buy) {
-                //     console.log("you're in luck")
-                // } else {
-                //     console.log("so sorry!");
-                // }
+                    // if (chosenItem.stock_quantity <= answer.units_to_buy) {
+                    //     console.log("you're in luck")
+                    // } else {
+                    //     console.log("so sorry!");
+                    // }
 
-                // console.log(answer)
+                    // console.log(answer)
 
-                // if (answer)
-            });
+                    // if (answer)
+                });
 
     })
 }
