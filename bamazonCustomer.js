9e86e316c3a63516d739645da99f958e9548c894
Dirk Kiesewetter <dirk.k1@ouotlook.com>
2018-12-14 10:00:28 -0500

    updated JS file console log syntax

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,50 +25,43 @@ connection.connect(function (err) {
     }
 })
 
-// function buyAgain() {
+function buyAgain() {
 
-//     inquirer.prompt([{
-//         name: 'choice',
-//         type: 'list',
-//         choices: ['YES', 'NO'],
-//         message: 'Do you want to buy anything else?\n Type CTRL-C to quit.\n',
-//         validate: function (value) {
-//             if (value == 'YES') {
-//                 console.log('i.d. called')
-//                 inventoryDisplay();
-//             } else {
-//                 console.log('c.e. called')
-//                 connection.end();
-//                 return;
-//             }
-//         }
-//     }])
-// }
+    inquirer.prompt([{
+        name: 'choice',
+        type: 'list',
+        choices: ['YES', 'NO'],
+        message: 'Do you want to buy anything else?\n Type CTRL-C/COMMAND-C to quit.\n',
+        validate: function (value) {
+            if (value == 'YES') {
+                console.log('i.d. called')
+                inventoryDisplay();
+            } else {
+                console.log('c.e. called')
+                connection.end();
+                return;
+            }
+        }
+    }])
+}
 
 function inventoryDisplay() {
 
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-        console.log('\n')
-        console.log('\x1b[36mItems available for sale:\x1b[0m');
+        console.log("\n\x1b[36mItems available for sale:\x1b[0m\n");
         console.table(results);
         console.log("---------------------------------------------------------");
 
         inquirer.prompt([{
                     name: 'choice',
                     type: 'input',
-                    message: 'What is the id of the product you want to purchase? [Type q to quit]',
+                    message: 'What is the item id of the product you want to purchase? [Type CTRL-c/COMMAND-c to quit]',
                     validate: function (value) {
-                        //prevents user from entering invalid input
                         if ((value > results.length) || (value < 1)) {
                             return false;
-                        }
-                        if ((isNaN(value) === false) && (value !== 'q')) {
+                        } else if (isNaN(value) === false) {
                             return true;
-                        }
-                        if (value === 'q') {
-                            connection.end()
-                            return false;
                         }
                         return false;
                     }
@@ -98,7 +91,7 @@ function inventoryDisplay() {
                     let currentItem = results[i];
                     if (currentItem.item_id == chosenItem) {
                         if (orderQty <= currentItem.stock_quantity) {
-                            console.log('\nYour order is approved. Your total cost is $' + (orderQty * currentItem.price).toFixed(2));
+                            console.log("\n\x1b[1m", "\x1b[44m", "Your order is approved. Your total cost is $" + (orderQty * currentItem.price).toFixed(2), "\x1b[0m");
 
                             // updates MySQL database
                             let updateQuery = "UPDATE products SET stock_quantity = " + (currentItem.stock_quantity - orderQty) +
